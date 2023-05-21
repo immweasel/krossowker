b8fd21efa01fd45b6ea1e8dc18fb9c118da86f0c
sol6nik <podymovv55@gmail.com>
2023-05-21 19:24:19 +0300

    header ended

---
+++ src/components/Catalog/Catalog.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from './Catalog.module.scss'
+
+function Catalog() {
+    return (
+        <div className={styles.catalog}>
+            <h1>Template Name Component</h1>
+        </div>
+    )
+}
+
+export default Catalog;
