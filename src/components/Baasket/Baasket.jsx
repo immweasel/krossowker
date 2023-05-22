1529cbd0865ee978bdbc2fba44b069fcac3e56f1
sol6nik <podymovv55@gmail.com>
2023-05-22 17:28:27 +0300

    Fixes

---
+++ src/components/Baasket/Baasket.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from './Baasket.module.scss'
+
+function Baasket() {
+    return (
+        <div className={styles.baasket}>
+            <h1 style={{
+                color: '#fff',
+            }}>Template Name Component</h1>
+        </div>
+    )
+}
+
+export default Baasket;
