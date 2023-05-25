ae0f12f9c549ce65810335e0a5f2c4ba3c256b45
Кира Максимова 211-323 <immweasel@yandex.ru>
2023-05-25 17:24:34 +0300

    feat: сделать работающий счётчик стоимости товара в корзине

--- src/components/Baasket/Baasket.jsx
+++ src/components/Baasket/Baasket.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+
 import styles from './Baasket.module.scss';
 import shoe1 from '../../assets/images/shoe1.jpg';
 import minus from '../../assets/images/Минус.svg';
 import plus from '../../assets/images/Плюс.svg';
 import krest from '../../assets/images/Крест.svg';
 
-function Baasket() {
+const ProductCounter = () => {
+  const [count, setCount] = useState(1);
+  const [price, setPrice] = useState(11500); 
+  const [totalPrice, setTotalPrice] = useState(11500);
+
+  const increment = () => {
+    setCount(count + 1);
+    setTotalPrice(totalPrice + price);
+  };
+
+  const decrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
+      setTotalPrice(totalPrice - price);
+    }
+  };
+
   return (
     <div className={styles.baasket}>
       <h1 className={styles.baasket__name}>КОРЗИНА</h1>
@@ -20,14 +37,14 @@ function Baasket() {
 							<p className={styles.baasket__photos_desc}>Размер: 42</p>
 							<p className={styles.baasket__photos_desc}>Артикул:4QEU84DJ324</p>
 							<div className={styles.baasket__photos_counter}>
-								<button className={styles.baasket__photos_btn}><img src={minus}></img></button>
-								<p className={styles.baasket__photos_count}>1</p>
-								<button className={styles.baasket__photos_btn}><img src={plus}></img></button>
+								<button className={styles.baasket__photos_btn} onClick={decrement}><img src={minus}></img></button>
+								<p className={styles.baasket__photos_count}>{count}</p>
+								<button className={styles.baasket__photos_btn} onClick={increment}><img src={plus}></img></button>
 							</div>
 						</div>
 						<div className={styles.baasket__photos_prices}>
 							<button className={styles.baasket__photos_btn}><img src={krest}></img></button>
-							<p className={styles.baasket__photos_price}>11 500 ₽</p>
+							<p className={styles.baasket__photos_price}>{totalPrice} ₽</p>
 						</div>
 					</div>
 				</div>
@@ -36,4 +53,4 @@ function Baasket() {
   )
 }
 
-export default Baasket;
+export default ProductCounter;
