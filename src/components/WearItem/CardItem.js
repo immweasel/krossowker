cf85d76c3d19d5ddf9147d2de2c525b6bb8ad851
sol6nik <podymovv55@gmail.com>
2023-05-25 12:02:35 +0300

    Some fix styles and added clothes

--- src/components/WearItem/CardItem.js
+++ src/components/WearItem/CardItem.js
@@ -22,7 +22,7 @@ function CardItem() {
             {shoes.map(item => (
                 <div className={styles.wearItem_card}>
                     <div className={styles.wearItem_card__cont}>
-                        <img src={item.img} alt={'shoe-puma'} className={styles.wearItem_img} />
+                        <img src={item.img} alt={item.img} className={styles.wearItem_card_img} />
                     </div>
                     <p className={styles.wearItem_title}>{item.name}</p>
                     <p className={styles.wearItem_categ}>{item.category}</p>
