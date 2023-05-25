f007dbbc9c8b4ef450d92e916ddc4e03fa9462c9
sol6nik <podymovv55@gmail.com>
2023-05-25 11:41:02 +0300

    minus unuse component

--- src/components/WearItem/CardItem.js
+++ src/components/WearItem/CardItem.js
@@ -1,32 +1,34 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import styles from './WearItem.module.scss'
 import axios from "axios";
 
-import ShoePuma from '../../assets/images/shoe1.png'
-
 function CardItem() {
-    // const [shoes, setShoes] = useState([])
-    //
-    // useEffect(() => {
-    //     const fetchShoes = async () => {
-    //         try {
-    //             const res = await axios.get('http://localhost:3001/wear')
-    //             setShoes(res.data)
-    //         } catch (e) {
-    //             console.log('Error: ', e)
-    //         }
-    //     }
-    //     fetchShoes()
-    // })
+    const [shoes, setShoes] = useState([])
+
+    useEffect(() => {
+        const fetchShoes = async () => {
+            try {
+                const res = await axios.get('http://localhost:3001/wear')
+                setShoes(res.data)
+            } catch (e) {
+                console.log('Error: ', e)
+            }
+        }
+        fetchShoes()
+    }, [])
 
     return (
         <>
-            <div className={styles.wearItem_card}>
-                <img src={ShoePuma} alt={'shoe-puma'}/>
-                <p className={styles.wearItem_title}>Vans Ultrarange Exo Hi</p>
-                <p className={styles.wearItem_categ}>Мужские кроссовки</p>
-                <p className={styles.wearItem_price}>11 500 ₽</p>
-            </div>
+            {shoes.map(item => (
+                <div className={styles.wearItem_card}>
+                    <div className={styles.wearItem_card__cont}>
+                        <img src={item.img} alt={'shoe-puma'} className={styles.wearItem_img} />
+                    </div>
+                    <p className={styles.wearItem_title}>{item.name}</p>
+                    <p className={styles.wearItem_categ}>{item.category}</p>
+                    <p className={styles.wearItem_price}>{item.price} ₽</p>
+                </div>
+            ))}
         </>
     )
 }
