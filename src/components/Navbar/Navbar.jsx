91eb47f064c913a248dd1421471210c84cd99f10
sol6nik <podymovv55@gmail.com>
2023-05-31 18:08:45 +0300

    Auth

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Link, Routes, Route, useLocation } from 'react-router-dom';
+import React, {useState} from 'react';
+import {Link, Routes, Route, useLocation} from 'react-router-dom';
 import Menu from "../Menu/Menu";
 import styles from './Navbar.module.scss';
 
@@ -11,6 +11,8 @@ import SearchIcon from '../../assets/icons/search.svg';
 import Baasket from '../Baasket/Baasket';
 import Catalog from "../Catalog/Catalog";
 import WearPage from '../WearPage/WearPage';
+import Authorization from "../Authorization/Authorization";
+import Profile from "../Profile/Profile";
 
 function Navbar() {
     const [activePath, setActivePath] = useState('/');
@@ -25,7 +27,7 @@ function Navbar() {
         <>
             <nav className={styles.navbar}>
                 <Link to={'/'}>
-                    <img src={Logo} alt="logo_image" className={styles.navbar_logo} />
+                    <img src={Logo} alt="logo_image" className={styles.navbar_logo}/>
                 </Link>
                 <div className={styles.navbar__links_container}>
                     <Link
@@ -66,28 +68,29 @@ function Navbar() {
                 </div>
                 <div className={styles.navbar__container}>
                     <Link to={'/Search'}>
-                        <img src={SearchIcon} alt="SearchIcon" />
+                        <img src={SearchIcon} alt="SearchIcon"/>
                     </Link>
                     <Link to={'/Basket'}>
-                        <img src={BasketIcon} alt="BasketIcon" />
+                        <img src={BasketIcon} alt="BasketIcon"/>
                     </Link>
-                    <Link to={'/profile'}>
-                        <img src={ProfileIcon} alt="ProfileIcon" />
+                    <Link to={'/auth'}>
+                        <img src={ProfileIcon} alt="ProfileIcon"/>
                     </Link>
                 </div>
             </nav>
 
             <Routes>
-                <Route path={'/'} element={'Main page'} />
-                <Route path={'/catalog'} element={<Menu />} />
-                <Route path={'/delivery'} element={'delivery'} />
-                <Route path={'/about_us'} element={'about us'} />
-                <Route path={'/contact'} element={'contact'} />
-                <Route path={'/profile'} element={'profile'} />
-                <Route path={'/basket'} element={<Baasket />} />
-                <Route path={'/search'} element={'profile'} />
-                <Route path={'/catalog-items'} element={<Catalog />} />
-                <Route path='/product/:id' element={<WearPage />} />
+                <Route path={'/'} element={'Main page'}/>
+                <Route path={'/catalog'} element={<Menu/>}/>
+                <Route path={'/delivery'} element={'delivery'}/>
+                <Route path={'/about_us'} element={'about us'}/>
+                <Route path={'/contact'} element={'contact'}/>
+                <Route path={'/profile/:id'} element={<Profile/>}/>
+                <Route path={'/auth'} element={<Authorization/>}/>
+                <Route path={'/basket'} element={<Baasket/>}/>
+                <Route path={'/search'} element={'search'}/>
+                <Route path={'/catalog-items'} element={<Catalog/>}/>
+                <Route path={'/product/:id'} element={<WearPage/>}/>
             </Routes>
         </>
     );
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import styles from './Profile.module.scss'
-import { useLocation, useNavigate } from "react-router-dom";
+import {useParams} from "react-router-dom";
+import axios from "axios";
+
 
 function Profile() {
+    const {userId} = useParams()
+    const [user, setUser] = useState(null)
 
-    const location = useLocation()
-    const { user } = location.state
+    useEffect(() => {
+        const fetchUserData = async () => {
+            axios.get(`http://localhost:3001/users/${userId}`)
+                .then(res => {
+                    setUser(res.data)
+                })
+                .catch(e => {
+                    console.error('Error: ', e)
+                })
+        }
+
+        fetchUserData()
+    }, [userId])
 
     return (
         <div className={styles.profile}>
@@ -14,22 +29,13 @@ function Profile() {
             <div className={styles.profile__data_cantainer}>
                 <p style={{
                     color: '#fff'
-                }}>
-                    Имя:
-                    {user.name} </p>
-
+                }}>Имя: {user.name} </p>
+                {/*<p style={{*/}
+                {/*    color: '#fff'*/}
+                {/*}}>Номер телефона: {user.phone_num} </p>*/}
                 <p style={{
                     color: '#fff'
-                }}>
-                    Номер телефона:
-                    <span>{user.phone_num}</span>
-                </p>
-
-                <p style={{
-                    color: '#fff'
-                }}>
-                    Почта:
-                    {user.email} </p>
+                }}>Почта: {user.email} </p>
             </div>
         </div>
     )
